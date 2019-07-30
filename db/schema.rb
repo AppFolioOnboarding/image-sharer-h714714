ddf66b20fbd514e4602c352a23f646bccc8bfb01
Sean <cheng-hsiang.ho@appfolio.com>
2019-07-30 10:46:56 -0700

    sh - create migration and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_07_30_173314) do
+
+  create_table "images", force: :cascade do |t|
+    t.text "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
