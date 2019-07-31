f47f3cd76f9d7b469259d248fdba4e55e771f881
Sean <cheng-hsiang.ho@appfolio.com>
2019-07-30 17:54:09 -0700

    sh - modify validation of url

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 require 'uri'
 class Image < ApplicationRecord
     validates :image_url, presence: true
-    validates_format_of :image_url, :with => URI.regexp
+    validates_format_of :image_url, :with => %r{^(http|https)://[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$}ix, multiline: true
 end
