518458cf71e02aa96fb5699a6b37d466fd025f35
Sean <cheng-hsiang.ho@appfolio.com>
2019-07-31 09:58:39 -0700

    sh - handle the url sent by user

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,16 @@ class ImagesController < ApplicationController
   def new
     @image = Image.new
   end
+
+  def create
+    @image = Image.new(image_url: params[:image][:image_url])
+
+    if @image.valid?
+      @image.save!
+      redirect_to images_path(@image.id)
+    else
+      render 'new'
+    end
+  end
+
 end
--- test/controllers/ImagesController_test.rb
+++ test/controllers/ImagesController_test.rb
@@ -5,4 +5,17 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     get root_path
     assert_response :ok
   end
+
+  def test_create_valid_url
+    assert_difference 'Image.count', 1 do
+      post images_path, params: { 'image' => { 'image_url' => 'http://e.com' } }
+    end
+  end
+
+  def test_create_invalid_url
+    assert_no_difference 'Image.count' do
+      post images_path, params: { 'image' => { 'image_url' => '123' } }
+    end
+  end
+
 end
