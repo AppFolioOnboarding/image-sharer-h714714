456e207c9f39cce024c939cb429166cc66074dd5
Sean <cheng-hsiang.ho@appfolio.com>
2019-08-01 10:47:10 -0700

    sh - add default images in databsae

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Image.create!(
+  [
+    { image_url: 'https://tineye.com/images/widgets/mona.jpg' },
+    { image_url: 'http://oi67.tinypic.com/2mg9fs8.jpg' },
+    { image_url: 'http://oi64.tinypic.com/2eamloy.jpg' },
+    { image_url: 'http://i66.tinypic.com/ivg7iu.jpg' },
+    { image_url: 'http://i67.tinypic.com/28a11js.jpg' },
+    { image_url: 'http://i66.tinypic.com/2mg8nsy.jpg' },
+    { image_url: 'http://i65.tinypic.com/2mg8oqh.jpg' },
+    { image_url: 'http://i66.tinypic.com/2mg9g7p.jpg' },
+    { image_url: 'http://i67.tinypic.com/2h4bbe1.jpg' },
+    { image_url: 'http://i66.tinypic.com/2h4babn.jpg' },
+    { image_url: 'http://i64.tinypic.com/2h4bxhh.jpg' },
+    { image_url: 'http://i68.tinypic.com/2h4bxvo.jpg' },
+    { image_url: 'http://i66.tinypic.com/ivg7iu.jpg' },
+    { image_url: 'http://i66.tinypic.com/ivg7iu.jpg' },
+    { image_url: 'http://i66.tinypic.com/ivg7iu.jpg' },
+    { image_url: 'http://i66.tinypic.com/ivg7iu.jpg' },
+    { image_url: 'http://i66.tinypic.com/ivg7iu.jpg' },
+    { image_url: 'http://i66.tinypic.com/ivg7iu.jpg' },
+    { image_url: 'http://i66.tinypic.com/ivg7iu.jpg' },
+    { image_url: 'http://i66.tinypic.com/ivg7iu.jpg' },
+    { image_url: 'http://i66.tinypic.com/ivg7iu.jpg' }
+  ]
+)
