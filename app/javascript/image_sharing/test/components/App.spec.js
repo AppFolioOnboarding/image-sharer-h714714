2f9eabcdd8a5c42d25761769028153fdcde9ab63
Sean <cheng-hsiang.ho@appfolio.com>
2019-08-06 16:55:40 -0700

    sh - create app component with feedback form and footer

--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -1,13 +1,16 @@
 /* eslint-env mocha */
 
 import assert from 'assert';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import App from '../../components/App';
 
 describe('<App />', () => {
   it('should render correctly', () => {
-    const wrapper = mount(<App />);
-    assert(wrapper.contains('Tell us what you think'));
+    const wrapper = shallow(<App />);
+    assert.strictEqual(wrapper.find('Header').length, 1);
+    assert.strictEqual(wrapper.find('Header').prop('title'), 'Tell us what you think');
+    assert.strictEqual(wrapper.find('FeedbackForm').length, 1);
+    assert.strictEqual(wrapper.find('Footer').length, 1);
   });
 });
