8646f693d3fdc4e09b2b63fffb5fab792fe7cffa
Sean <cheng-hsiang.ho@appfolio.com>
2019-08-07 18:07:16 -0700

    sh - show message for both valid and invalid submission

--- app/javascript/image_sharing/components/FeedbackForm.js
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import { Form, FormGroup, Container, Row, Col, Label, Input, Button } from 'reactstrap';
+import { Alert, Button, Col, Container, Form, FormGroup, Input, Label, Row } from 'reactstrap';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+import * as api from '../utils/helper';
 
+@observer
 export default class FeedbackForm extends React.Component {
+  @observable name = '';
+  @observable comment = '';
+  @observable alert = '';
+  @observable alertColor;
+
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    return api.post('/api/feedbacks', { feedback: { name: this.name, comment: this.comment } }).then(() => {
+      this.name = '';
+      this.comment = '';
+      this.alert = 'Submit successfully !';
+      this.alertColor = 'success';
+    }).catch(() => {
+      this.alert = 'A name and a comment is required !';
+      this.alertColor = 'danger';
+    });
+  }
+
+
   render() {
     return (
       <Container>
         <Row>
-          <Col xs="3" />
-          <Col xs="6">
-            <Form>
+          <Col xs='3' />
+          <Col xs='6'>
+            <Form onSubmit={this.handleSubmit} >
+              { this.alert && <Alert color={this.alertColor}>{this.alert}</Alert> }
               <FormGroup>
                 <Label>Your name:</Label>
-                <Input type="email" />
+                <Input value={this.name} onChange={(e) => { this.name = e.target.value; }} />
               </FormGroup>
               <FormGroup>
                 <Label>Comments:</Label>
-                <Input type="textarea" />
+                <Input type="textarea" value={this.comment} onChange={(e) => { this.comment = e.target.value; }} />
               </FormGroup>
+              <Button color="primary" size="sm">Submit</Button>
             </Form>
-            <Button color="primary" size="sm">Submit</Button>
           </Col>
         </Row>
       </Container>
--- app/javascript/image_sharing/test/components/FeedbackForm.spec.js
+++ app/javascript/image_sharing/test/components/FeedbackForm.spec.js
@@ -3,7 +3,9 @@
 import assert from 'assert';
 import { shallow } from 'enzyme';
 import React from 'react';
+import sinon from 'sinon';
 import FeedbackForm from '../../components/FeedbackForm';
+import * as api from '../../utils/helper';
 
 describe('<FeedbackForm />', () => {
   it('should render correctly', () => {
@@ -19,11 +21,60 @@ describe('<FeedbackForm />', () => {
     assert.strictEqual(wrapper.find('Label').first().children().text(), 'Your name:');
     assert.strictEqual(wrapper.find('Label').last().children().text(), 'Comments:');
     assert.strictEqual(wrapper.find('Input').length, 2);
-    assert.strictEqual(wrapper.find('Input').first().prop('type'), 'email');
     assert.strictEqual(wrapper.find('Input').last().prop('type'), 'textarea');
     assert.strictEqual(wrapper.find('Button').length, 1);
     assert.strictEqual(wrapper.find('Button').prop('color'), 'primary');
     assert.strictEqual(wrapper.find('Button').prop('size'), 'sm');
     assert.strictEqual(wrapper.find('Button').children().text(), 'Submit');
+  });
+
+  it('should call post when submit', () => {
+    const wrapper = shallow(<FeedbackForm />);
+    wrapper.instance().name = 'my name';
+    wrapper.instance().comment = 'my comment';
+    wrapper.update();
+
+    const preventDefaultSpy = sinon.spy();
+    const mockEvent = { preventDefault: preventDefaultSpy };
+
+    const stub = sinon.stub(api, 'post');
+    stub.resolves();
+    wrapper.instance().handleSubmit(mockEvent).then(() => {
+      wrapper.update();
+      assert(preventDefaultSpy.calledOnce);
+      assert.strictEqual(wrapper.instance().alertColor, 'success');
+    });
+    stub.restore();
+  });
+
+  it('should raise warning when submit invalid form', () => {
+    const wrapper = shallow(<FeedbackForm />);
+    wrapper.instance().comment = 'my comment';
+    wrapper.update();
+
+    const preventDefaultSpy = sinon.spy();
+    const mockEvent = { preventDefault: preventDefaultSpy };
+
+    const stub = sinon.stub(api, 'post');
+    stub.rejects();
+    wrapper.instance().handleSubmit(mockEvent).then(() => {
+      wrapper.update();
+      assert(preventDefaultSpy.calledOnce);
+      assert.strictEqual(wrapper.instance().alertColor, 'danger');
+    });
+    stub.restore();
+  });
+
+  it('should not render Alert when alert is nil', () => {
+    const wrapper = shallow(<FeedbackForm />);
+    assert.strictEqual(wrapper.find('Alert').length, 0);
+  });
+
+  it('should change name when input changes', () => {
+    const wrapper = shallow(<FeedbackForm />);
+    const input = wrapper.find('Input').first();
+    input.simulate('change', { target: { value: 'something' } });
+
+    assert.strictEqual(wrapper.instance().name, 'something');
   });
 });
